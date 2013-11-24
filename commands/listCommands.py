7aebb25f8a92246f78280d2267ecbc9f705a0597
RenolY2 <debugyoshi@gmail.com>
2013-11-24 15:56:03 +0100

    Improved command list
    The list groups commands into Guest, Voiced, OP and Owner. Now it sends
    the command list in a Notice to the user, and it only lists the commands
    which the user is allowed to use.

--- commands/listCommands.py
+++ commands/listCommands.py
@@ -1,11 +1,25 @@
 ID = "commands"
-permission = 2
+permission = 0
+
+rankdict = {"@@" : 3, "@" : 2, "+" : 1, "" : 0}
+rankname = ("Guest", "Voiced", "OP", "Owner")
 
 def execute(self, name, params, channel, userdata, rank):
-    commands = ""
+    
+    perms = rankdict[rank]
+    group = {}
+    for i in range(perms+1):
+        group[i] = []
+         
     for cmd in self.commands:
-        commands += cmd + " | "
+        cmdrank = self.commands[cmd][0].permission
+        if perms >= cmdrank:
+            group[cmdrank].append(cmd)
+    
+            
         
-    self.sendChatMessage(self.send, channel, "Available commands:")   
-    self.sendChatMessage(self.send, channel, commands)
+    self.sendNotice(name, "Available commands:")   
+    for i in group:
+        group[i].sort()
+        self.sendNotice(name, "{0}: {1}".format(rankname[i], " | ".join(group[i])))
     
