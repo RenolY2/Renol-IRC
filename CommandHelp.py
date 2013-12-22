643ee4cc4406bc58209fd8afb59cdc8751356ae5
RenolY2 <debugyoshi@gmail.com>
2013-12-22 11:23:49 +0100

    Added documentation for Help Module
    The documentation uses commands/showHelp.py as an example for how to use
    the help module.

--- CommandHelp.py
+++ CommandHelp.py
@@ -1,5 +1,78 @@
 
+"""
+The Help Module allows the user to define a help entry for his command
+This help entry will be added to the internal Help Database which is looked
+up with the bot's help command.
+Users can also define descriptions for each argument their command takes, and they can flag
+the argument as optional or not. The help command will use this information to format the
+resulting text accordingly.
 
+Example of usage in commands/showHelp.py, with added comments:
+
+#We need to define a __initialize__ function so that the code is executed on startup:
+def __initialize__(self, Startup):
+
+    # self.helper is a HelpModule object.
+    # Using help.newHelp, we are creating a new HelpEntity object with the ID, i.e. the name of the command,
+    # as the first argument. In this case, ID is the string "help". 
+    # Using non-string values can have unforseen consequences, please use a string.
+    
+    entry = self.helper.newHelp(ID) 
+
+
+    # We add two lines of descriptions to our help entry. They will be put out as seperate
+    # NOTICE messages to the user when he looks up the help information.
+    
+    entry.addDescription("The 'help' command shows you the descriptions and arguments of commands that have added an entry to the internal Help Database.")
+    entry.addDescription("You can only view the help of a command if you are authorized to use the command.")
+    
+    
+    # We add two arguments, "command name" and "argument name", and their descriptions to the help entry.
+    # Please note that arguments can only have one line, although long lines will be broken into several 
+    # NOTICE messages later by the sendNotice function in the help command.
+    #
+    # We flag the "argument name" argument as optional, this only has an aesthetic function so that the user
+    # knows which arguments are required and which ones are optional and can be left out.
+    
+    entry.addArgument("command name", "The name of the command you want to know about.")
+    entry.addArgument("argument name", "The name of the argument you want to know about.", optional = True)
+    
+    
+    # We set the rank for the command information. Per default, the rank is already 0 for every HelpEntity object,
+    # but you want to change the rank value to a higher number to restrict unauthorized users from
+    # reading the description. You can set the rank value to a lower number than your command if you don't
+    # mind users reading the description of a command they cannot use.
+    
+    entry.rank = 0
+    
+    
+    # Finally, we register this entry with the bot's HelpModule object. This will add the entry to its
+    # internal Help Database which is used by the help command. The registerHelp method will raise a 
+    # RuntimeError if the command entry already exists, you may want to set overwrite = True if you don't mind
+    # overwriting the previous entry, e.g. on command reload.
+    
+    self.helper.registerHelp(entry, overwrite = True)
+
+
+
+
+# Commands that require more complicated help functions can define their own help handler.
+# In that case, the help command will pass on its arguments to the custom help handler function.
+#
+# The help command will still handle checking if the command exists in the internal database and
+# if the user is allowed to read the help information, please consider that when you define a 
+# function for showing information about the command.
+
+def helpHandler(self, name, params, channel, userdata, rank):
+    print "Hi, I am an example for a custom help handler!"
+    
+def __initialize__(self, Startup):
+    entry = self.helper.newHelp(commandName)
+    entry.setCustomHandler(helpHandler)
+    entry.rank = 0
+    self.helper.registerHelp(entry, overwrite = True)
+    
+"""
 
 class HelpEntity():
     def __init__(self, cmdname):
