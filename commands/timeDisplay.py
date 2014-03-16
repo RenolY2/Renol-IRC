22ed19941822dda801493dbdc78bdc121f0870cd
RenolY2 <debugyoshi@gmail.com>
2014-03-16 09:49:14 +0100

    Removed unix time display

--- commands/timeDisplay.py
+++ commands/timeDisplay.py
@@ -6,7 +6,4 @@ privmsgEnabled = True
 
 
 def execute(self, name, params, channel, userdata, rank, chan):
-    if len(params) > 0 and params[0] == "unix":
-        self.sendChatMessage(self.send, channel, "{0}".format(time.time()))
-    else:
-        self.sendChatMessage(self.send, channel, time.asctime())
+    self.sendChatMessage(self.send, channel, time.asctime())
