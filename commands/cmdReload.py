2372413fd7ae665560451bcf8f57dbfd8fe244ff
RenolY2 <debugyoshi@gmail.com>
2014-04-11 21:26:58 +0200

    Removed ability to reload ALL commands

--- commands/cmdReload.py
+++ commands/cmdReload.py
@@ -15,7 +15,7 @@ def execute(self, name, params, channel, userdata, rank):
         
         try:
             if not callable(self.commands[cmd][0].__initialize__):
-               self.commands[cmd][0].__initialize__ = False
+                self.commands[cmd][0].__initialize__ = False
         except AttributeError:
             self.commands[cmd][0].__initialize__ = False
         else:
@@ -26,17 +26,12 @@ def execute(self, name, params, channel, userdata, rank):
     elif len(params) > 0 and params[0] not in self.commands:
         self.sendChatMessage(self.send, channel, "Command does not exist")
     else:
-        self.sendChatMessage(self.send, channel, "Reloading..")
-        self.commands = self.__LoadModules__("commands")
-        for cmd in self.commands:
-            if self.commands[cmd][0].__initialize__ != False:
-                self.commands[cmd][0].__initialize__(self, False)
-        self.sendChatMessage(self.send, channel, "Done!")
+        self.sendMessage(channel, "Please specify a command.")
 
 def __initialize__(self, Startup):
     entry = self.helper.newHelp(ID)
     
-    entry.addDescription("The 'reload' command allows you to reload all commands at once, or one specific command at a time.")
+    entry.addDescription("The 'reload' command allows you to reload specific commands. All changes made to the file will take effect.")
     entry.addArgument("command name", "The name of the command you want to reload", optional = True)
     entry.rank = permission
     
