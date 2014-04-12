2484f8f084a730d89101198bb4346468f3aac951
RenolY2 <debugyoshi@gmail.com>
2014-04-12 13:20:18 +0200

    Very important for WolfAPI command

---
+++ commands/miscLib/xml_to_dict.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+from xml.etree import cElementTree as ET
+
+# Code snippet from K3---rnc for the conversion of XML to the python dictionary format.
+# http://stackoverflow.com/questions/2148119/how-to-convert-an-xml-string-to-a-dictionary-in-python/10077069#10077069
+def etree_to_dict(t):
+    d = {t.tag: {} if t.attrib else None}
+    children = list(t)
+    
+    if children:
+        dd = defaultdict(list)
+        for dc in map(etree_to_dict, children):
+            for k, v in dc.iteritems():
+                dd[k].append(v)
+        d = {t.tag: {k:v[0] if len(v) == 1 else v for k, v in dd.iteritems()}}
+    if t.attrib:
+        d[t.tag].update(('@' + k, v) for k, v in t.attrib.iteritems())
+    if t.text:
+        text = t.text.strip()
+        if children or t.attrib:
+            if text:
+                d[t.tag]['#text'] = text
+        else:
+            d[t.tag] = text
+    return d
+                
+def xml_to_dict(xmlstring):
+    tree = ET.XML(xmlstring)
+    xmldict = etree_to_dict(tree)
+    
+    return xmldict
